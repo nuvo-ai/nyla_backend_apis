4d6c9f3df74585ac31bbc495b73a482419cd99f8
Peter <iriogbepeter22@gmail.com>
2025-09-02 14:31:35 +0100

    .

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    Log::info('Broadcast auth attempt', [
+        'auth_user_id' => $user->id,
+        'channel_id'   => $id,
+    ]);
+
     return (int) $user->id === (int) $id;
 });
+
