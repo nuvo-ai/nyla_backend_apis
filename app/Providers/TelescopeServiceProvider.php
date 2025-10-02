8b29e8c52bae400b4f99d63de65f4b5c9f7e255b
Peter <iriogbepeter22@gmail.com>
2025-10-02 20:44:05 +0100

    add email to telescope provider

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -56,9 +56,10 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
-                'iriogbepeter22@gmail.com',
-            ]);
+            // return in_array($user->email, [
+            //     'iriogbepeter22@gmail.com',
+            // ]);
+            return true;
         });
     }
 }
